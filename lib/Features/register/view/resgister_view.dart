963cd990eb40145a8e93ed2c2db4767b2a61854a
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-12-14 11:34:05 +0200

    trigger register  cubit part two

--- lib/Features/register/view/resgister_view.dart
+++ lib/Features/register/view/resgister_view.dart
@@ -1,14 +1,36 @@
+import 'package:atw_task1_app/Features/register/cubits/cubit/register_cubit.dart';
+import 'package:atw_task1_app/core/helpers/show_snack_bar_function.dart';
 import 'package:atw_task1_app/core/widgets/register_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class ResgisterView extends StatelessWidget {
-  const ResgisterView({super.key});
-
+  ResgisterView({super.key});
+  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFFF3F3F3),
-      body: RegisterBody(),
+    return BlocConsumer<RegisterCubit, RegisterState>(
+      listener: (context, state) {
+        if (state is RegisterLoading) {
+          isLoading = true;
+        } else if (state is RegisterSuccess) {
+          Navigator.pop(context);
+          isLoading = false;
+        } else if (state is RegisterFailure) {
+          showSnachBarFun(context, state.erorrMessage);
+          isLoading = false;
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+          inAsyncCall: isLoading,
+          child: const Scaffold(
+            backgroundColor: Color(0xFFF3F3F3),
+            body: RegisterBody(),
+          ),
+        );
+      },
     );
   }
 }
