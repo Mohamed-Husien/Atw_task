5f405d672bef6fe84cfbc50dcf62a6353ccf6f8e
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-12-14 11:46:14 +0200

    refactor sing in cubit

--- lib/Features/singin/cubits/sing_in_cubit/singin_cubit.dart
+++ lib/Features/singin/cubits/sing_in_cubit/singin_cubit.dart
@@ -8,16 +8,21 @@ class SinginCubit extends Cubit<SinginState> {
   SinginCubit() : super(SinginInitial());
   Future<void> loginUser(
       {required String email, required String password}) async {
-    emit(SinginInitial());
+    emit(SinginInLoading());
     try {
-      UserCredential user = await FirebaseAuth.instance
+      final credential = await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: email, password: password);
-
       emit(SinginInSuccess());
-    } on Exception catch (e) {
-      emit(
-        SinginInFailure(erorrMessage: 'something went wrong '),
-      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        emit(SinginInFailure(erorrMessage: 'No user found for that email.'));
+      } else if (e.code == 'wrong-password') {
+        emit(SinginInFailure(
+            erorrMessage: 'Wrong password provided for that user.'));
+      }
+    } catch (e) {
+      emit(SinginInFailure(
+          erorrMessage: 'There was an error please try again!'));
     }
   }
 }
